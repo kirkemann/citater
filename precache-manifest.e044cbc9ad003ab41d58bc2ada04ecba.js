b2fff1cc6b2f7bb97e2727957d304ad0b6dac397
Kirkemann <kirkemann35@gmail.com>
2019-09-16 11:54:32 +0200

    Initial commit

---
+++ precache-manifest.e044cbc9ad003ab41d58bc2ada04ecba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dffa4c2d5637a0525c41f2131fef1fde",
+    "url": "/opgave_react_1/index.html"
+  },
+  {
+    "revision": "932ddec9a3e12c64e5c8",
+    "url": "/opgave_react_1/static/css/main.60b713d8.chunk.css"
+  },
+  {
+    "revision": "a9d38d585ff54180a14d",
+    "url": "/opgave_react_1/static/js/2.4343f44c.chunk.js"
+  },
+  {
+    "revision": "932ddec9a3e12c64e5c8",
+    "url": "/opgave_react_1/static/js/main.05901aca.chunk.js"
+  },
+  {
+    "revision": "2f5e22da1fb50efef68a",
+    "url": "/opgave_react_1/static/js/runtime~main.4f37243b.js"
+  }
+]);
